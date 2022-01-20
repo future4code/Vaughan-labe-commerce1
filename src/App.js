695fde32e29eb5f97904893349ceea4355e47f4b
ViniciusBarbosa <92874088+viniciuscicone@users.noreply.github.com>
2022-01-20 14:54:49 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,95 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+   state = {
+      produto: [ 
+         {
+            id: Date.now(),
+            name: "Foguete da Missão Apollo 11",
+            value: 400.0,
+            imageUrl: "https://picsum.photos/200/200",
+         },
+         {
+            id: Date.now(),
+            name: "Foguete da Missão Apollo 11",
+            value: 10000.0,
+            imageUrl: "https://picsum.photos/200/200",
+         },
+
+      ],
+
+      valorInput: ""
+   }
+
+
+
+   render() {
+
+
+
+
+
+
+
+      return (
+         <div className="App">
+
+
+
+
+            <header className='Topo'>
+            
+
+               <div className='QuantidadeProdutos'>Quantidade de produtos</div>
+               <div className='SeletorOrdenario'>Seletor
+               
+               <select className='Seletor'>
+               <option>Nenhum</option>
+               <option>Crescente</option>
+               <option>Decrescente</option>
+               </select>
+               
+               </div>
+            </header>
+
+
+
+            <main className="App-header">
+
+
+
+            
+
+             <div className="FiltroDiv" >Filtro<br/>
+
+
+
+             <input placeholder='Entrada' type="number"></input>
+             <input placeholder='Entrada' type="number"></input>
+             <input placeholder='Entrada' type="text"></input>
+
+
+
+             </div>
+             <div className='ProdutosDiv'>Produtos
+             
+             
+             
+             </div>
+             
+             <div className='CarrinhoDiv'>Carrinho
+             
+             
+             
+             </div>
+            </main>
+         </div>
+      );
+
+   }
+
 }
 
 export default App;
