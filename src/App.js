71d6a56fee03800a47fb670e5880c3ce41cd9318
ViniciusBarbosa <92874088+viniciuscicone@users.noreply.github.com>
2022-01-22 13:29:47 -0300

    Adicionar e remover itens do carrinho adicionado no site

--- src/App.js
+++ src/App.js
@@ -1,54 +1,56 @@
 import React from 'react';
 import './App.css';
 
+
+
 class App extends React.Component {
 
    state = {
       produtos: [
          {
-            id: Date.now(),
+            id: Math.random(),
             name: "Foguete Apollo 11",
             value: 400.0,
             imageUrl: "https://picsum.photos/200/200",
          },
          {
-            id: Date.now(),
+            id: Math.random(),
             name: "Foguete da Missão Apollo 1",
             value: 100.0,
             imageUrl: "https://picsum.photos/200/200?a=8",
          },
          {
-            id: Date.now(),
+            id: Math.random(),
             name: "Foguete joaozinho 9",
             value: 10.0,
             imageUrl: "https://picsum.photos/200/200?a=7",
          },
          {
-            id: Date.now(),
+            id: Math.random(),
             name: "Foguete bolinha 11",
             value: 20.0,
             imageUrl: "https://picsum.photos/200/200?a=4",
          },
          {
-            id: Date.now(),
+            id: Math.random(),
             name: "Foguete smalatak 11",
             value: 2000.0,
             imageUrl: "https://picsum.photos/200/200?a=5",
          },
          {
-            id: Date.now(),
+            id: Math.random(),
             name: "Foguete abdubadala quinto",
             value: 200.0,
             imageUrl: "https://picsum.photos/200/200?a=6",
          },
          {
-            id: Date.now(),
+            id: Math.random(),
             name: "Foguete ronaldinho 44",
             value: 1.0,
             imageUrl: "https://picsum.photos/200/200?a=3",
          },
          {
-            id: Date.now(),
+            id: Math.random(),
             name: "Foguete mars ultimate 11",
             value: 8000.0,
             imageUrl: "https://picsum.photos/200/200?a=2",
@@ -61,66 +63,104 @@ class App extends React.Component {
 
 
       carrinho: [
-         {
-            /* 
-            name: "",
-            value: Number,
-            quantidade: Number */
-         },
+
       ]
    }
 
 
-   adicionarProduto = () => {
-      //LOGICA DO CARRINHO ENTRA AQUI 
+   adicionarProduto = (productId) => {
 
-      console.log("Adicionou no carrinho !")
+      const produtoNoCarrinho = this.state.carrinho.find(produto => productId === produto.id)
 
-      const produtoCarrinho = {
 
-         name: "",
-         value: Number,
-         quantidade: Number
+      if (produtoNoCarrinho) {
+
+         const acrescentaCarrinho = this.state.carrinho.map(produto => {
+            if (productId === produto.id) {
+               return {
+                  ...produto,
+                  quantidade: produto.quantidade + 1,
+
+               }
+
+            }
+
+            return produto
+         })
+
+         this.setState({ carrinho: acrescentaCarrinho })
+
+      } else {
+
+         const adicionarProdutoNaoExistente = this.state.produtos.find(produto => productId === produto.id)
+
+         const novoProdutoIndoCarrinho = [...this.state.carrinho, { ...adicionarProdutoNaoExistente, quantidade: 1 }]
+
+         this.setState({ carrinho: novoProdutoIndoCarrinho })
 
       }
 
-
-      this.setState({ carrinho: produtoCarrinho })
-
    }
+
+
+
+
+
+
+   removeProdutoCarrinho = (produtoId) => {
+
+      const novoProduto = this.state.carrinho.map((produto) => {
+
+         if(produto.id === produtoId) {
+            return {
+               ...produto,
+               quantidade: produto.quantidade - 1
+            }
+         }
+         return produto
+
+      }).filter((produto) => produto.quantidade > 0  )
+
+
+      this.setState({ carrinho: novoProduto })
+   }
+
+
 
 
 
 
 
    render() {
-      const Produto = this.state.produtos.map((produto, x) => {
+
+
+
+      const Carrinho = this.state.carrinho.map((produtoCarrinho) => {
 
          return (
-            
-               <div key={x} className='cardsProdutos'>
-                  <img className='CardImagem' src={produto.imageUrl} alt='Imagem bonita'></img>
-                  <a className='CardNome' href="/#">{produto.name}</a>
-                  <a className='CardValor'/><a className='NomeValor'>Valor R$ {produto.value}</a> 
-                  <button className='BotaoProduto' >Adicionar no carrinho</button>
-               </div>
-            
+
+            <div>         
+                  <div className='ProdutoNoCarrinho'>{produtoCarrinho.name} X{produtoCarrinho.quantidade}<br/>
+                  R$ {produtoCarrinho.value * produtoCarrinho.quantidade}
+                  <button className='botao' onClick={ () => this.removeProdutoCarrinho(produtoCarrinho.id)} >Remover</button></div>        
+            </div>
+         )
+      });
+
+      const Produto = this.state.produtos.map((produto) => {
+
+         return (
+
+            <div key={produto.id} className='cardsProdutos'>
+               <img className='CardImagem' src={produto.imageUrl} alt='Imagem bonita'></img>
+               <a className='CardNome' href="/#" value={produto.name} >{produto.name}</a>
+               <a className='CardValor' /><a className='NomeValor' >Valor R$ {produto.value}</a>
+               <button className='BotaoProduto' onClick={() => this.adicionarProduto(produto.id)} >Adicionar no carrinho</button>
+            </div>
+
          );
       });
-/* 
-      const Carrinho = this.state.carrinho.map((produto) => {
 
-         return (
-
-            <>
-
-
-            </>
-
-         )
-
-      })
- */
       return (
          <div className="App">
 
@@ -158,11 +198,9 @@ class App extends React.Component {
                </div>
 
                <div className='CarrinhoDiv'>Carrinho
-                  <div className='ItemNoCarrinho'>
-                     <button className='BotaoRemover' >Remover</button>
-                  </div>
 
-                  {''}
+                  {Carrinho}
+
 
                </div>
             </main>
